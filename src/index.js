acebb62b43703ede061ff588e2a6039651da3ffa
Rohit Bhati <rohitbhati@live.in>
2017-07-31 17:24:42 +0530

    Add navbar and sidebar with routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css'
+
+import { NavBar } from './components'
+import Routes from './components/Routes'
+
+const RouterComp = () => {
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Routes />
+      </div>
+    </Router>
+  )
+}
+
+ReactDOM.render(<RouterComp />, document.getElementById('root'))
+registerServiceWorker()
