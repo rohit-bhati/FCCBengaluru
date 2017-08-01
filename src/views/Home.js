7a24626c27eae2ecadfebbcc0c42d6949e9fa844
Rohit Bhati <rohitbhati@live.in>
2017-08-01 15:06:49 +0530

    add basic carousel

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,60 @@
 import React, { Component } from 'react'
-import './../styles/App.css'
+import Slider from 'react-slick'
+import { Helmet } from 'react-helmet'
+import PropTypes from 'prop-types'
+import { withStyles, createStyleSheet } from 'material-ui/styles'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
+const styleSheet = createStyleSheet('Home', {
+  container: {
+    width: '90%',
+    margin: '0 auto'
+  }
+})
 class Home extends Component {
   render () {
-    return <div>Home Page</div>
+    const classes = this.props.classes
+    const images = [
+      'meetup01',
+      'meetup02',
+      'meetup03',
+      'meetup04',
+      'meetup05',
+      'meetup06',
+      'meetup07'
+    ]
+    const settings = {
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      slidesToShow: 1,
+      slidesToScroll: 1
+      // centerPadding: '60px'
+    }
+    return (
+      <div className={classes.container}>
+        <Helmet title='Home -- FCC Bengaluru' />
+        <Slider {...settings}>
+          {images.map((image, index) => {
+            return (
+              <img
+                key={index}
+                src={
+                  process.env.PUBLIC_URL + '/images/carousel/' + image + '.png'
+                }
+                alt='carousel-images'
+              />
+            )
+          })}
+        </Slider>
+      </div>
+    )
   }
 }
 
-export default Home
+Home.PropTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styleSheet)(Home)
